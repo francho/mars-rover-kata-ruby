a818caa250560b26436d304398edddad4d8cc0b0
Francho Joven <lab@francho.org>
2015-04-18 00:35:52 +0200

    Adds move forward command

--- lib/mars_rover.rb
+++ lib/mars_rover.rb
@@ -2,8 +2,19 @@ class MarsRover
   attr_accessor :direction
   attr_accessor :position
 
-  def initialize(initial_direction, initial_position)
+  def initialize(initial_direction, initial_x, initial_y)
     @direction=initial_direction
-    @position=initial_position
+    @position={x: initial_x, y: initial_y}
   end
+
+  def execute(command)
+    move_forward if command=='f'
+  end
+
+  protected
+
+  def move_forward
+    @position[:y]+=1
+  end
+
 end
--- spec/mars_rover_spec.rb
+++ spec/mars_rover_spec.rb
@@ -1,9 +1,14 @@
 require 'rspec'
 require_relative '../lib/mars_rover'
 
+# 3
+# 2
+# 1
+# 0 1 2 3
+
 describe 'MarsRovers' do
 
-  let(:rover) { MarsRover.new('N', [0,0]) }
+  let(:rover) { MarsRover.new('N', 0, 0) }
 
   context 'properties' do
     it 'has a direction' do
@@ -16,7 +21,14 @@ describe 'MarsRovers' do
 
     it 'starts at concrete position and direction' do
       expect(rover.direction).to eql 'N'
-      expect(rover.position).to eql [0,0]
+      expect(rover.position).to include(x: 0, y: 0)
+    end
+  end
+
+  context 'basic movement' do
+    it 'moves forward' do
+      rover.execute('f')
+      expect(rover.position).to include(x: 0, y: 1)
     end
   end
 

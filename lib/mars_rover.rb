adce2e6876b5a25b13f583d90349d2683f6bbcc7
Francho Joven <lab@francho.org>
2015-04-17 23:58:07 +0200

    Add base properties

--- lib/mars_rover.rb
+++ lib/mars_rover.rb
@@ -1,2 +1,9 @@
 class MarsRover
+  attr_accessor :direction
+  attr_accessor :position
+
+  def initialize(initial_direction, initial_position)
+    @direction=initial_direction
+    @position=initial_position
+  end
 end
--- spec/mars_rover_spec.rb
+++ spec/mars_rover_spec.rb
@@ -1,9 +1,23 @@
-require 'test/spec'
+require 'rspec'
+require_relative '../lib/mars_rover'
 
-context 'MarsRover' do
+describe 'MarsRovers' do
 
-  specify 'should do something' do
+  let(:rover) { MarsRover.new('N', [0,0]) }
 
-    true.should == false
+  context 'properties' do
+    it 'has a direction' do
+      expect(rover).to respond_to :direction
+    end
+
+    it 'has coordinates' do
+      expect(rover).to respond_to :position
+    end
+
+    it 'starts at concrete position and direction' do
+      expect(rover.direction).to eql 'N'
+      expect(rover.position).to eql [0,0]
+    end
   end
+
 end
